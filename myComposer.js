47cf91631131f64c442d53a8131f5d27750c24d5
omar-afifi <omar.afifi@vodafone.com>
2022-09-28 09:54:18 +0200

    fix myComposer

--- myComposer.js
+++ myComposer.js
@@ -3,13 +3,13 @@ import _ from 'lodash'
 export default (resolvers, resolversComposition) => {
   const customResolver = (validators, resolver) => async (root, args, context, info) => {
     for (const validator of validators) {
-      validator(root, args, context, info)
+      await validator(root, args, context, info)
     }
     return resolver(root, args, context, info)
   }
   for (const path in resolversComposition) {
     const validators = resolversComposition[path]
-    const mappedResolver = resolvers[path]
+    const mappedResolver = _.get(resolvers, path)
     _.set(resolvers, path, customResolver(validators, mappedResolver))
   }
   return resolvers
